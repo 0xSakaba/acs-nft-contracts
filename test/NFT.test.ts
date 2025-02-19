92d101e1d1fa92c6f458fe03e993e12042d709ed
yihsiu <s30401s@gmail.com>
2025-02-19 19:14:46 +0900

    feat: create unit test

--- test/NFT.test.ts
+++ test/NFT.test.ts
@@ -1,227 +1,179 @@
 import { expect } from "chai";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 import { Signer } from "ethers";
-import { AstarNFTCommon, AstarNFTRare } from "../typechain-types";
+import { MockErc20, SonovaACSContract } from "../typechain-types";
 
 describe("NuushiNFT", function () {
-  let AstarNFTCommon: AstarNFTCommon;
-  let AstarNFTRare: AstarNFTRare;
+  let SonovaACS: SonovaACSContract;
+  let MockPaymentToken: MockErc20;
   let owner: Signer;
-  let addr1: Signer;
+  let minter: Signer;
   let addr2: Signer;
 
+  const baseTokenURI =
+    "ipfs:/bafkreicmhkxixbihhywewbvob63us56fsmpwd6cw7sv7gx3hkilv6yroli";
+
   beforeEach(async function () {
-    [owner, addr1, addr2] = await hre.ethers.getSigners();
-    const AstarNFTCommonFactory = await hre.ethers.getContractFactory(
-      "AstarNFTCommon"
-    );
-    AstarNFTCommon = await AstarNFTCommonFactory.deploy("https://example.com/");
+    [owner, minter, addr2] = await hre.ethers.getSigners();
 
-    const AstarNFTRareFactory = await hre.ethers.getContractFactory(
-      "AstarNFTRare"
+    const MockErc20Factory = await hre.ethers.getContractFactory("MockErc20");
+
+    MockPaymentToken = await MockErc20Factory.deploy("MockErc20", "MERC");
+
+    await MockPaymentToken.mint(
+      await minter.getAddress(),
+      ethers.parseEther("1000000")
     );
-    AstarNFTRare = await AstarNFTRareFactory.deploy("https://example.com/");
+
+    const SonovaACSFactory = await hre.ethers.getContractFactory(
+      "SonovaACSContract"
+    );
+
+    SonovaACS = await SonovaACSFactory.deploy(
+      baseTokenURI,
+      await MockPaymentToken.getAddress()
+    );
   });
 
-  it("Should deploy the contract", async function () {
-    expect(await AstarNFTCommon.name()).to.equal(
-      "Astar Expansion: Soneium Edition - Common"
-    );
-    expect(await AstarNFTCommon.symbol()).to.equal("AESEC");
+  it("Should deploy the contract & check balance", async function () {
+    expect(await SonovaACS.name()).to.equal("Sonova ACS edition");
+    expect(await SonovaACS.symbol()).to.equal("SACSE");
 
-    expect(await AstarNFTRare.name()).to.equal(
-      "Astar Expansion: Soneium Edition - Rare"
-    );
-    expect(await AstarNFTRare.symbol()).to.equal("AESER");
+    expect(
+      await MockPaymentToken.balanceOf(await minter.getAddress())
+    ).to.equal(ethers.parseEther("1000000"));
   });
 
-  it("COMMON: Should set the start and end timestamp", async function () {
+  it("Should set the start and end timestamp", async function () {
     const block = await hre.ethers.provider.getBlock("latest");
     const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
 
-    expect(await AstarNFTCommon.startTimestamp()).to.equal(
-      currentBlockTime - 1
-    );
+    expect(await SonovaACS.startTimestamp()).to.equal(currentBlockTime - 1);
 
-    expect(await AstarNFTCommon.endTimestamp()).to.equal(currentBlockTime + 5);
+    expect(await SonovaACS.endTimestamp()).to.equal(currentBlockTime + 5);
   });
 
-  it("COMMON: Should mint NFTs within the sale period", async function () {
+  it("Should mint NFTs within the sale period", async function () {
     const block = await hre.ethers.provider.getBlock("latest");
     const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
 
-    await AstarNFTCommon.connect(addr1).mint();
-    expect(await AstarNFTCommon.totalSupply()).to.equal(1);
-  });
-
-  it("COMMON: Should block mint NFTs due to one wallet one mint", async function () {
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
-
-    await AstarNFTCommon.connect(addr1).mint();
-    await expect(AstarNFTCommon.connect(addr1).mint()).to.be.revertedWith(
-      "Max mint limit reached for wallet"
+    // should approve the contract to spend the token first
+    await MockPaymentToken.connect(minter).approve(
+      await SonovaACS.getAddress(),
+      ethers.parseEther("10")
     );
+    await SonovaACS.connect(minter).mint();
+    expect(await SonovaACS.totalSupply()).to.equal(1);
+
+    // expect the balance of the minter to be reduced
+    expect(
+      await MockPaymentToken.balanceOf(await minter.getAddress())
+    ).to.equal(ethers.parseEther("999990"));
   });
 
-  it("COMMON: Should block mint NFTs due to sale has not started", async function () {
+  it("Should block mint NFTs due to sale has not started", async function () {
     // await hre.ethers.provider.send("evm_increaseTime", [1]); // Increase time by 1 second
     // await hre.ethers.provider.send("evm_mine", []);
-    await expect(AstarNFTCommon.connect(addr1).mint()).to.be.revertedWith(
+    await expect(SonovaACS.connect(minter).mint()).to.be.revertedWith(
       "Sale has not started"
     );
   });
 
-  it("COMMON: Should block mint NFTs Sale has ended", async function () {
+  it(" Should block mint NFTs Sale has ended", async function () {
     const block = await hre.ethers.provider.getBlock("latest");
     const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
     await hre.ethers.provider.send("evm_increaseTime", [10]); // Increase time by 1 second
     await hre.ethers.provider.send("evm_mine", []);
-    await expect(AstarNFTCommon.connect(addr1).mint()).to.be.revertedWith(
+    await expect(SonovaACS.connect(minter).mint()).to.be.revertedWith(
       "Sale has ended"
     );
   });
 
-  it("COMMON: Should get token URI", async function () {
+  it("Should get token URI", async function () {
     const block = await hre.ethers.provider.getBlock("latest");
     const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
 
-    await AstarNFTCommon.connect(addr1).mint();
-    expect(await AstarNFTCommon.tokenURI(1)).to.be.equal(
-      "https://example.com/"
+    // should approve the contract to spend the token first
+    await MockPaymentToken.connect(minter).approve(
+      await SonovaACS.getAddress(),
+      ethers.parseEther("10")
+    );
+    await SonovaACS.connect(minter).mint();
+    expect(await SonovaACS.tokenURI(1)).to.be.equal(baseTokenURI);
+  });
+
+  it("Should get minted count", async function () {
+    const block = await hre.ethers.provider.getBlock("latest");
+    const currentBlockTime = block?.timestamp!;
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
+
+    // should approve the contract to spend the token first
+    await MockPaymentToken.connect(minter).approve(
+      await SonovaACS.getAddress(),
+      ethers.parseEther("10")
+    );
+    await SonovaACS.connect(minter).mint();
+
+    expect(await SonovaACS.mintedCount(await minter.getAddress())).to.be.equal(
+      1
     );
   });
 
-  it("COMMON: Should get minted count", async function () {
+  it("Should withdraw the token by owner after mint", async function () {
+    // set the start and end timestamp
     const block = await hre.ethers.provider.getBlock("latest");
     const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
-    await AstarNFTCommon.connect(addr1).mint();
+    await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+    await SonovaACS.setEndTimestamp(currentBlockTime + 5);
 
-    expect(
-      await AstarNFTCommon.mintedCount(await addr1.getAddress())
-    ).to.be.equal(1);
-  });
-
-  it("COMMON: Should change max mint per wallet", async function () {
-    await AstarNFTCommon.setMaxMintPerWallet(2);
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    await AstarNFTCommon.setStartTimestamp(currentBlockTime - 1);
-    await AstarNFTCommon.setEndTimestamp(currentBlockTime + 5);
-    await AstarNFTCommon.connect(addr1).mint();
-    await AstarNFTCommon.connect(addr1).mint();
-
-    expect(
-      await AstarNFTCommon.mintedCount(await addr1.getAddress())
-    ).to.be.equal(2);
-  });
-
-  it("RARE: Should get current sale day = 0", async function () {
-    expect(await AstarNFTRare.getCurrentSaleDay()).to.be.equal(0);
-  });
-
-  it("RARE: Should get current sale day = 5", async function () {
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    // day , start time, end time
-    await AstarNFTRare.setSalePeriod(5, currentBlockTime, currentBlockTime + 5);
-
-    expect(await AstarNFTRare.getCurrentSaleDay()).to.be.equal(5);
-  });
-
-  it("RARE: Should block mint => Not in sale period", async function () {
-    await expect(AstarNFTRare.connect(addr1).mint()).to.be.revertedWith(
-      "Not in sale period"
+    // should approve the contract to spend the token first
+    await MockPaymentToken.connect(minter).approve(
+      await SonovaACS.getAddress(),
+      ethers.parseEther("10")
     );
+    await SonovaACS.connect(minter).mint();
+
+    await SonovaACS.connect(owner).withdrawToken();
+
+    expect(
+      await MockPaymentToken.balanceOf(await owner.getAddress())
+    ).to.be.equal(ethers.parseEther("10"));
   });
 
-  // this one should change the contract MAX_MINT_PER_DAY to 1 then test
-  // it("RARE: Should block mint => Daily mint limit reached", async function () {
+  /// need to change the max supply to 1 in contract
+  // it("Should reach limit if the token MAX_SUPPLY = 1 ", async function () {
+  //   // set the start and end timestamp
   //   const block = await hre.ethers.provider.getBlock("latest");
   //   const currentBlockTime = block?.timestamp!;
-  //   // day , start time, end time
-  //   await AstarNFTRare.setSalePeriod(
-  //     5,
-  //     currentBlockTime,
-  //     currentBlockTime + 300
+  //   await SonovaACS.setStartTimestamp(currentBlockTime - 1);
+  //   await SonovaACS.setEndTimestamp(currentBlockTime + 10);
+
+  //   // 1st
+  //   // should approve the contract to spend the token first
+  //   await MockPaymentToken.connect(minter).approve(
+  //     await SonovaACS.getAddress(),
+  //     ethers.parseEther("10")
+  //   );
+  //   await SonovaACS.connect(minter).mint();
+
+  //   // 2nd mint should reach the limit
+  //   // should approve the contract to spend the token first
+  //   await MockPaymentToken.connect(minter).approve(
+  //     await SonovaACS.getAddress(),
+  //     ethers.parseEther("10")
   //   );
 
-  //   await AstarNFTRare.connect(addr1).mint();
-
-  //   await expect(AstarNFTRare.connect(addr2).mint()).to.be.revertedWith(
-  //     "Daily mint limit reached"
+  //   await expect(SonovaACS.connect(minter).mint()).to.be.revertedWith(
+  //     "Max supply reached"
   //   );
   // });
-
-  it("RARE: Should block mint => Daily mint limit reached #1", async function () {
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    // day , start time, end time
-    await AstarNFTRare.setSalePeriod(
-      5,
-      currentBlockTime,
-      currentBlockTime + 300
-    );
-
-    await AstarNFTRare.connect(addr1).mint();
-
-    await expect(AstarNFTRare.connect(addr1).mint()).to.be.revertedWith(
-      "Max mint limit reached for wallet"
-    );
-  });
-
-  it("RARE: Should block mint => Daily mint limit reached #2", async function () {
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    // day , start time, end time
-    await AstarNFTRare.setSalePeriod(
-      5,
-      currentBlockTime,
-      currentBlockTime + 300
-    );
-
-    await AstarNFTRare.connect(addr1).mint();
-
-    await AstarNFTRare.setSalePeriod(
-      1,
-      currentBlockTime,
-      currentBlockTime + 300
-    );
-
-    await expect(AstarNFTRare.connect(addr1).mint()).to.be.revertedWith(
-      "Max mint limit reached for wallet"
-    );
-  });
-
-  it("RARE: Should mint rare nft then check mint per wallet and mintedCount of period", async function () {
-    const block = await hre.ethers.provider.getBlock("latest");
-    const currentBlockTime = block?.timestamp!;
-    // day , start time, end time
-    await AstarNFTRare.setSalePeriod(
-      5,
-      currentBlockTime,
-      currentBlockTime + 300
-    );
-
-    await AstarNFTRare.connect(addr1).mint();
-    await AstarNFTRare.connect(addr2).mint();
-    await AstarNFTRare.connect(owner).mint();
-
-    expect(await AstarNFTRare.getMintedCount(5)).to.be.equal(3);
-    expect(
-      await AstarNFTRare.mintedCount(await addr1.getAddress())
-    ).to.be.equal(1);
-  });
 });
